2168aa03902eb88c03057043420213c1998c8f65
abdoulaadim <77145529+Abdoulaadim@users.noreply.github.com>
2022-04-21 10:04:43 +0000

    heroku

--- src/main/java/com/mabrouki/smart_house/config/SecurityConfig.java
+++ src/main/java/com/mabrouki/smart_house/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.mabrouki.smart_house.services.UserService;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,6 +23,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.userDetailsService = userDetailsService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/user/register");
+    }
+
 
 
     @Override
